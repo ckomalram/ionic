589eb3a3853967697fc22e7097a9483f00f83886
Carlyle Komalram <glaw14@gmail.com>
2022-03-21 23:22:18 -0500

    fin seccion 4

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [];
+  constructor() {
+    this.componentes = [
+      {
+        icon: 'logo-apple-ar',
+        name: 'Action Sheet',
+        redirectTo: '/action-sheet'
+      },
+      {
+        icon: 'logo-apple-appstore',
+        name: 'Alert',
+        redirectTo: '/alert'
+      }
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+}
